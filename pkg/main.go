0228d738a5c7ac6b0f3355689ce1b8f6a16654be
ismail simsek <ismailsimsek09@gmail.com>
2026-04-28 13:36:41 +0200

    Gate 0 readiness fixes (#23)

--- pkg/main.go
+++ pkg/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	if err := datasource.Manage("grafana-opentsdb-datasource", opentsdb.NewDatasource, datasource.ManageOpts{}); err != nil {
+	if err := datasource.Manage("opentsdb", opentsdb.NewDatasource, datasource.ManageOpts{}); err != nil {
 		log.DefaultLogger.Error(err.Error())
 		os.Exit(1)
 	}
